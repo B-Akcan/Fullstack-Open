d325b0f207c663094cf25ae7fbcbc1b4e9d4e52a
Batuhan Akçan <batuhanakcan38@gmail.com>
2024-08-04 17:08:01 +0300

    Added part4

---
+++ part4/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require("mongoose")
+const logger = require("./utils/logger.js")
+const config = require("./utils/config.js")
+const middleware = require("./utils/middleware.js")
+const blogsRouter = require("./controllers/blogs.js")
+
+logger.info("Connecting to", config.MONGODB_URI)
+
+mongoose.set('strictQuery', false)
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connection successful")
+  })
+  .catch(() => {
+    logger.error("connection failed")
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use("/api/blogs", blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
